0eb1738d642819edbda4761c9802f6188c4e4296
mingyan.wmy <wenmingyan1990@163.com>
2024-10-14 16:19:10 +0800

    地面判断可视化

---
+++ LegendOfTheWarrior/Assets/Scripts/Player/PhysicsCheck.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+public class PhyciseCheck : MonoBehaviour
+{
+    /// <summary>
+    /// 检测地面的半径
+    /// </summary>
+    public float checkRadius = 0.2f;
+    public Vector2 bottomOffset;
+    public LayerMask groundLayer;
+    /// <summary>
+    /// 是否在地面
+    /// </summary>
+    public bool isOnGround;
+    private void Update()
+    {
+        Check();
+    }
+    public void Check()
+    {
+        // - 检测是否在地面
+        isOnGround = Physics2D.OverlapCircle((Vector2)transform.position + bottomOffset, checkRadius, groundLayer);
+    }
+    /// <summary>
+    /// Callback to draw gizmos only if the object is selected.
+    /// </summary>
+    void OnDrawGizmosSelected()
+    {
+     Gizmos.DrawWireSphere((Vector2)transform.position + bottomOffset,checkRadius);   
+    }
+}
--- LegendOfTheWarrior/Assets/Scripts/Player/PlayerController.cs
+++ LegendOfTheWarrior/Assets/Scripts/Player/PlayerController.cs
@@ -1,11 +1,17 @@
 
+using System;
+using System.Runtime.Remoting.Messaging;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
     public PlayerInputSystem inputControl;
     public Vector2 inputDirection;
+    private PhyciseCheck phycisCheck;
+    [Header("基本参数")]
     public float speed = 260;
+    public float jumpForce = 16;
     private Rigidbody2D rigidbody2d;
     /// <summary>
     /// Awake is called when the script instance is being loaded.
@@ -14,14 +20,29 @@ public class PlayerController : MonoBehaviour
     {
         inputControl = new PlayerInputSystem();
         rigidbody2d = GetComponent<Rigidbody2D>();
+        phycisCheck = GetComponent<PhyciseCheck>();
+
     }
     private void OnEnable()
     {
         inputControl.Enable();
+        // 监听按下按键跳跃
+        inputControl.GamePlay.Jump.started += onReceiveGamePlayJump;
     }
     private void OnDisable()
     {
         inputControl.Disable();
+        inputControl.GamePlay.Jump.started -= onReceiveGamePlayJump;
+    }
+
+    private void onReceiveGamePlayJump(InputAction.CallbackContext context)
+    {
+        if (phycisCheck.isOnGround)
+        {
+            // 给一个向上的冲力
+            rigidbody2d.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
+        }
+
     }
 
     private void Update()
@@ -38,20 +59,18 @@ public class PlayerController : MonoBehaviour
     private void Move()
     {
         // 获取到input的数值
-        inputDirection = inputControl.GamePlay.Move.ReadValue<Vector2>();
+        Vector2 inputDirection = inputControl.GamePlay.Move.ReadValue<Vector2>();
         rigidbody2d.velocity = new Vector2(inputDirection.x * speed * Time.deltaTime, rigidbody2d.velocity.y);
         // 人物翻转
-
+        float scaleX = Mathf.Abs(transform.localScale.x);
         if (inputDirection.x < 0)
         {
-            transform.localScale = new Vector3(-1, 1, 1);
+            transform.localScale = new Vector3(-scaleX, transform.localScale.y, transform.localScale.z);
         }
         else
         {
-            transform.localScale = new Vector3(1, 1, 1);
+            transform.localScale = new Vector3(scaleX, transform.localScale.y, transform.localScale.z);
         }
-
-
     }
 
 }
