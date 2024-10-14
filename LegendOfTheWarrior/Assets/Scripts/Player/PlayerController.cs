fb1d850ce48d5d2748f7d72959c6a0c09e382bee
mingyan.wmy <wenmingyan1990@163.com>
2024-10-14 14:42:08 +0800

    左右移动

--- LegendOfTheWarrior/Assets/Scripts/Player/PlayerController.cs
+++ LegendOfTheWarrior/Assets/Scripts/Player/PlayerController.cs
@@ -1,19 +1,20 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     public PlayerInputSystem inputControl;
     public Vector2 inputDirection;
+    public float speed = 260;
+    private Rigidbody2D rigidbody2d;
     /// <summary>
     /// Awake is called when the script instance is being loaded.
     /// </summary>
     void Awake()
     {
         inputControl = new PlayerInputSystem();
+        rigidbody2d = GetComponent<Rigidbody2D>();
     }
-
     private void OnEnable()
     {
         inputControl.Enable();
@@ -25,8 +26,32 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
-        // 获取到的数值
+
+
+    }
+    private void FixedUpdate()
+    {
+        // 固定时钟频率
+        Move();
+    }
+
+    private void Move()
+    {
+        // 获取到input的数值
         inputDirection = inputControl.GamePlay.Move.ReadValue<Vector2>();
+        rigidbody2d.velocity = new Vector2(inputDirection.x * speed * Time.deltaTime, rigidbody2d.velocity.y);
+        // 人物翻转
+
+        if (inputDirection.x < 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+        else
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+
+
     }
 
 }
