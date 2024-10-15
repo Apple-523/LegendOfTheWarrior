94cd7d4c38cc1a18072f39ef7bf920e807417964
mingyan.wmy <wenmingyan1990@163.com>
2024-10-15 15:17:03 +0800

    主人公动画

--- LegendOfTheWarrior/Assets/Scripts/Player/PlayerAnimation.cs
+++ LegendOfTheWarrior/Assets/Scripts/Player/PlayerAnimation.cs
@@ -2,10 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class PlayerAnimation : MonoBehaviour
 {
     private Animator animator;
-    private void Awake() {
+    private Rigidbody2D rigidbody2d;
+    public static string kAnimVelocityX = "velocityX";
+    public static string kAnimVelocityY = "velocityY";
+    public static string kAnimIsOnGround = "isOnGround";
+
+    private PhyciseCheck phyciseCheck;
+    
+
+    private void Awake()
+    {
         animator = GetComponent<Animator>();
+        rigidbody2d = GetComponent<Rigidbody2D>();
+        phyciseCheck = GetComponent<PhyciseCheck>();
+    }
+    private void Update()
+    {
+        animator.SetFloat(kAnimVelocityX, Mathf.Abs(rigidbody2d.velocity.x));
+        animator.SetFloat(kAnimVelocityY, rigidbody2d.velocity.y);
+        animator.SetBool(kAnimIsOnGround,phyciseCheck.isOnGround);
     }
 }
